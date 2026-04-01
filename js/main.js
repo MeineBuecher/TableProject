f2db71b7ebb540241c9a18dc84b30f93711633a8
MeineBuecher <christian.mayer@online.de>
2026-04-01 15:32:33 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+import { setStatus } from "./utils.js";
+import { createRoom } from "./room.js";
 
+document.addEventListener("DOMContentLoaded", () => {
+  setStatus("FaceProject gestartet");
+
+  window.createRoom = () => {
+    const name = document.getElementById("nameInput").value;
+    createRoom(name);
+  };
+});
