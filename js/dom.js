73ea3d00129544a111f9285deb23fe5f618f2520
MeineBuecher <christian.mayer@online.de>
2026-04-01 15:30:12 +0200

    Update dom.js

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,14 @@
+export function getEl(id) {
+  return document.getElementById(id);
+}
 
+export const getParticipantsBox = () => getEl("participantsBox");
+export const getWorkerBox = () => getEl("workerBox");
+export const getOwnerBox = () => getEl("ownerBox");
+export const getChatMessagesBox = () => getEl("chatMessages");
+export const getChatInput = () => getEl("chatInput");
+export const getFilesArea = () => getEl("filesArea");
+export const getImagesArea = () => getEl("imagesArea");
+export const getTextsArea = () => getEl("textsArea");
+export const getShareScreenBtn = () => getEl("shareScreenBtn");
+export const getExpertPanel = () => getEl("expertPanel");
