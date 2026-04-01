f4ade5ecf353d14905b597a2c8129e495356bda5
MeineBuecher <christian.mayer@online.de>
2026-04-01 15:31:46 +0200

    Update screen.js

--- js/screen.js
+++ js/screen.js
@@ -1,1 +1,9 @@
+import { screenSlots } from "./state.js";
 
+export function resetScreens() {
+  screenSlots.forEach(s => {
+    s.owner = null;
+    s.stream = null;
+    s.active = false;
+  });
+}
